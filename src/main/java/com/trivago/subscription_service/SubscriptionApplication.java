d1995643f37cb7f6881f2f23711d2586e3c03b2e
Ravikant Pal <ravikant.pal@timesinternet.in>
2024-05-31 18:02:41 +0530

    Initial commit: project setup with initial dependencies

---
+++ src/main/java/com/trivago/subscription_service/SubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.trivago.subscription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApplication.class, args);
+	}
+
+}
